0da55b3b52676aab6de7be31d851efbd763470d0
Jeff Johnson <jeff@fpgadeveloper.com>
2014-08-06 15:14:15 -0400

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 zedboard-axi-dma
 ================
 
-Demonstration of the AXI DMA engine on the ZedBoard
+Demonstration project for the AXI DMA Engine on the ZedBoard
+
+### Description
+
+This project demonstrates the use of the AXI DMA Engine IP for transfering
+data between a custom IP block and memory.
+
+### Requirements
+
+* Vivado 2014.2
+* MicroZed
+
+### License
+
+Feel free to modify the code for your specific application.
+
+### Fork and share
+
+If you port this project to another hardware platform, please send me the
+code or push it onto GitHub and send me the link so I can post it on my
+website. The more people that benefit, the better.
+
+### About the author
+
+I'm an FPGA consultant and I provide FPGA design services and training to
+innovative companies around the world. I believe in sharing knowledge and
+I regularly contribute to the open source community.
+
+Jeff Johnson
+http://www.fpgadeveloper.com
---
+++ Vivado/build.tcl
@@ -0,0 +1,89 @@
+#
+# build.tcl: Tcl script for re-creating project 'zedboard_axi_dma'
+#
+#*****************************************************************************************
+
+# Set the reference directory to where the script is
+set origin_dir [file dirname [info script]]
+
+# Create project
+create_project zedboard_axi_dma $origin_dir/zedboard_axi_dma
+
+# Set the directory path for the new project
+set proj_dir [get_property directory [current_project]]
+
+# Set project properties
+set obj [get_projects zedboard_axi_dma]
+set_property "board_part" "em.avnet.com:zed:part0:1.0" $obj
+set_property "default_lib" "xil_defaultlib" $obj
+set_property "simulator_language" "Mixed" $obj
+set_property "target_language" "VHDL" $obj
+
+# Create 'sources_1' fileset (if not found)
+if {[string equal [get_filesets -quiet sources_1] ""]} {
+  create_fileset -srcset sources_1
+}
+
+# Set 'sources_1' fileset properties
+set obj [get_filesets sources_1]
+set_property "top" "design_1_wrapper" $obj
+
+# Create 'constrs_1' fileset (if not found)
+if {[string equal [get_filesets -quiet constrs_1] ""]} {
+  create_fileset -constrset constrs_1
+}
+
+# Set 'constrs_1' fileset object
+set obj [get_filesets constrs_1]
+
+# Empty (no sources present)
+
+# Set 'constrs_1' fileset properties
+set obj [get_filesets constrs_1]
+
+# Create 'sim_1' fileset (if not found)
+if {[string equal [get_filesets -quiet sim_1] ""]} {
+  create_fileset -simset sim_1
+}
+
+# Set 'sim_1' fileset object
+set obj [get_filesets sim_1]
+# Empty (no sources present)
+
+# Set 'sim_1' fileset properties
+set obj [get_filesets sim_1]
+set_property "top" "design_1_wrapper" $obj
+
+# Create 'synth_1' run (if not found)
+if {[string equal [get_runs -quiet synth_1] ""]} {
+  create_run -name synth_1 -part xc7z020clg484-1 -flow {Vivado Synthesis 2014} -strategy "Vivado Synthesis Defaults" -constrset constrs_1
+} else {
+  set_property strategy "Vivado Synthesis Defaults" [get_runs synth_1]
+  set_property flow "Vivado Synthesis 2014" [get_runs synth_1]
+}
+set obj [get_runs synth_1]
+
+# set the current synth run
+current_run -synthesis [get_runs synth_1]
+
+# Create 'impl_1' run (if not found)
+if {[string equal [get_runs -quiet impl_1] ""]} {
+  create_run -name impl_1 -part xc7z020clg484-1 -flow {Vivado Implementation 2014} -strategy "Vivado Implementation Defaults" -constrset constrs_1 -parent_run synth_1
+} else {
+  set_property strategy "Vivado Implementation Defaults" [get_runs impl_1]
+  set_property flow "Vivado Implementation 2014" [get_runs impl_1]
+}
+set obj [get_runs impl_1]
+
+# set the current impl run
+current_run -implementation [get_runs impl_1]
+
+puts "INFO: Project created:zedboard_axi_dma"
+
+# Create block design
+source $origin_dir/src/bd/design_1.tcl
+
+# Generate the wrapper
+set design_name [get_bd_designs]
+make_wrapper -files [get_files $design_name.bd] -top -import
+
